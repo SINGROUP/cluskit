5a138d4ee35ce03beb5b7816389d79568549ef91
marc.o.j.jaeger <marc.o.j.jaeger@gmail.com>
2018-06-12 17:17:34 +0300

    created python wrapper for the functions getSurf, getEta1,2,3.

---
+++ clusgeo.py
@@ -0,0 +1,308 @@
+import numpy as np 
+import ase, ase.io
+import os, argparse
+from ctypes import *
+import soaplite
+import pathlib
+
+def _format_ase2clusgeo(obj, all_atomtypes=[]):
+    """ Takes an ase Atoms object and returns numpy arrays and integers
+    which are read by the internal clusgeo. Apos is currently a flattened
+    out numpy array
+    """
+    #atoms metadata
+    totalAN = len(obj)
+    if all_atomtypes:
+        atomtype_set = set(all_atomtypes)
+    else:
+        atomtype_set = set(obj.get_atomic_numbers())
+    num_atomtypes = len(atomtype_set)
+
+    atomtype_lst = np.sort(list(atomtype_set))
+    n_atoms_per_type_lst = []
+    pos_lst = []
+    for atomtype in atomtype_lst:
+        condition = obj.get_atomic_numbers() == atomtype
+        pos_onetype = obj.get_positions()[condition]
+        n_onetype = pos_onetype.shape[0]
+
+        # store data in lists
+        pos_lst.append(pos_onetype)
+        n_atoms_per_type_lst.append(n_onetype)
+
+    typeNs = n_atoms_per_type_lst
+    Ntypes = len(n_atoms_per_type_lst)
+    atomtype_lst
+    Apos = np.concatenate(pos_lst).ravel()
+    return Apos, typeNs, Ntypes, atomtype_lst, totalAN
+
+def _get_supercell(obj, rCut=5.0):
+    rCutHard = rCut + 5; # Giving extra space for hard cutOff
+    """ Takes atoms object (with a defined cell) and a radial cutoff.
+    Returns a supercell centered around the original cell
+    generously extended to contain all spheres with the given radial
+    cutoff centered around the original atoms.
+    """
+
+    xyz_arr = np.abs(np.diag(obj.get_cell()))
+    cell_images = np.ceil(rCutHard/xyz_arr)
+    nx = int(cell_images[0])
+    ny = int(cell_images[1])
+    nz = int(cell_images[2])
+
+    suce = obj * (1 + 2*nx, 1+ 2*ny,1+2*nz)
+    shift = obj.get_cell()
+
+    shifted_suce = suce.copy()
+    shifted_suce.translate(-shift[0]*nx -shift[1]*ny - shift[1]*nz)
+
+    return suce
+
+def get_surface_atoms(obj):
+    """Takes an ASE atoms object.
+    Returns an array of indices of surface atoms.
+    """
+    # get clusgeo internal format for c-code
+    py_totalAN = len(obj)
+    py_surfAtoms = np.zeros(py_totalAN, dtype=int)
+    pos = obj.get_positions()
+    py_x, py_y, py_z = pos[:,0], pos[:,1], pos[:,2]
+
+    # convert int to c_int
+    totalAN = c_int(py_totalAN)
+    #convert int array to c_int array
+    surfAtoms = (c_int * py_totalAN)(*py_surfAtoms)
+
+    # convert to c_double arrays
+    x = (c_double * py_totalAN)(*py_x)
+    y = (c_double * py_totalAN)(*py_y)
+    z = (c_double * py_totalAN)(*py_z)
+
+    path_to_so = os.path.dirname(os.path.abspath(__file__))
+
+    libclusgeo = CDLL(path_to_so + '/libclusgeo3')
+    libclusgeo.findSurf.argtypes = [POINTER (c_double),POINTER (c_double), POINTER (c_double), POINTER (c_int), c_int]
+    libclusgeo.findSurf.restype = c_int
+
+    Nsurf = libclusgeo.findSurf(x, y, z, surfAtoms, totalAN)
+
+    py_surfAtoms = np.ctypeslib.as_array( surfAtoms, shape=(py_totalAN))
+    py_surfAtoms = py_surfAtoms[:Nsurf]
+
+    return py_surfAtoms
+
+def get_top_sites(obj, surfatoms):
+    """Takes an ASE atoms object and an array of surface atom indices as input
+    Returns a 2D-array of top site positions.
+    """
+    # get clusgeo internal format for c-code
+    py_totalAN = len(obj)
+    py_Nsurf = len(surfatoms)
+    py_surfAtoms = np.zeros(py_totalAN, dtype=int)
+    py_surfAtoms[:py_Nsurf] = surfatoms
+    pos = obj.get_positions()
+    py_x, py_y, py_z = pos[:,0], pos[:,1], pos[:,2]
+
+    # convert int to c_int
+    totalAN = c_int(py_totalAN)
+    Nsurf = c_int(py_Nsurf)
+
+    #convert int array to c_int array
+    surfAtoms = (c_int * py_totalAN)(*py_surfAtoms)
+
+    # convert to c_double arrays
+    x = (c_double * py_totalAN)(*py_x)
+    y = (c_double * py_totalAN)(*py_y)
+    z = (c_double * py_totalAN)(*py_z)
+
+    surfH = (c_double*(py_Nsurf * 3  ) )()
+
+    path_to_so = os.path.dirname(os.path.abspath(__file__))
+
+    libclusgeo = CDLL(path_to_so + '/libclusgeo3')
+    libclusgeo.getEta1.argtypes = [POINTER (c_double),POINTER (c_double), POINTER (c_double), POINTER (c_double), POINTER (c_int), c_int, c_int]
+
+    libclusgeo.getEta1(surfH, x, y, z, surfAtoms, Nsurf, totalAN)
+
+    py_surfH = np.ctypeslib.as_array( surfH, shape=(py_Nsurf*3))
+    py_surfH = py_surfH.reshape((py_Nsurf,3))
+
+    return py_surfH
+
+
+def get_edge_sites(obj, surfatoms):
+    """Takes an ASE atoms object and an array of surface atom indices as input
+    Returns a 2D-array of top site positions.
+    """
+    # get clusgeo internal format for c-code
+    py_totalAN = len(obj)
+    py_Nsurf = len(surfatoms)
+    py_surfAtoms = np.zeros(py_totalAN, dtype=int)
+    py_surfAtoms[:py_Nsurf] = surfatoms
+    pos = obj.get_positions()
+    py_x, py_y, py_z = pos[:,0], pos[:,1], pos[:,2]
+
+    # convert int to c_int
+    totalAN = c_int(py_totalAN)
+    Nsurf = c_int(py_Nsurf)
+
+    #convert int array to c_int array
+    surfAtoms = (c_int * py_totalAN)(*py_surfAtoms)
+
+    # convert to c_double arrays
+    x = (c_double * py_totalAN)(*py_x)
+    y = (c_double * py_totalAN)(*py_y)
+    z = (c_double * py_totalAN)(*py_z)
+
+    surfH = (c_double*(py_Nsurf * 3 * py_Nsurf  ) )()
+
+    path_to_so = os.path.dirname(os.path.abspath(__file__))
+
+    libclusgeo = CDLL(path_to_so + '/libclusgeo3')
+    libclusgeo.getEta2.argtypes = [POINTER (c_double),POINTER (c_double), POINTER (c_double), POINTER (c_double), POINTER (c_int), c_int, c_int]
+
+    Nedge = libclusgeo.getEta2(surfH, x, y, z, surfAtoms, Nsurf, totalAN)
+
+    py_surfH = np.ctypeslib.as_array( surfH, shape=(py_Nsurf*3* py_Nsurf))
+    py_surfH = py_surfH.reshape((py_Nsurf*py_Nsurf,3))
+    py_surfH = py_surfH[:Nedge] 
+
+    return py_surfH
+
+def get_hollow_sites(obj, surfatoms):
+    """Takes an ASE atoms object and an array of surface atom indices as input
+    Returns a 2D-array of top site positions.
+    """
+    # get clusgeo internal format for c-code
+    py_totalAN = len(obj)
+    py_Nsurf = len(surfatoms)
+    py_surfAtoms = np.zeros(py_totalAN, dtype=int)
+    py_surfAtoms[:py_Nsurf] = surfatoms
+    pos = obj.get_positions()
+    py_x, py_y, py_z = pos[:,0], pos[:,1], pos[:,2]
+
+    # convert int to c_int
+    totalAN = c_int(py_totalAN)
+    Nsurf = c_int(py_Nsurf)
+
+    #convert int array to c_int array
+    surfAtoms = (c_int * py_totalAN)(*py_surfAtoms)
+
+    # convert to c_double arrays
+    x = (c_double * py_totalAN)(*py_x)
+    y = (c_double * py_totalAN)(*py_y)
+    z = (c_double * py_totalAN)(*py_z)
+
+    surfH = (c_double*(py_Nsurf * 3 * py_Nsurf  ) )()
+
+    path_to_so = os.path.dirname(os.path.abspath(__file__))
+
+    libclusgeo = CDLL(path_to_so + '/libclusgeo3')
+    libclusgeo.getEta3.argtypes = [POINTER (c_double),POINTER (c_double), POINTER (c_double), POINTER (c_double), POINTER (c_int), c_int, c_int]
+
+    Nhollow = libclusgeo.getEta3(surfH, x, y, z, surfAtoms, Nsurf, totalAN)
+
+    py_surfH = np.ctypeslib.as_array( surfH, shape=(py_Nsurf*3* py_Nsurf))
+    py_surfH = py_surfH.reshape((py_Nsurf*py_Nsurf,3))
+    py_surfH = py_surfH[:Nhollow] 
+
+    return py_surfH
+
+
+def write_all_sites(atoms, structure_name = "ref", path = "./"):
+    """Takes an ASE atoms object and a string of the name of the structure.
+    Returns None
+    It writes xyzfiles with top, edge and hollow sites into directories
+    """
+    surfatoms = get_surface_atoms(atoms)
+    topHxyz = get_top_sites(atoms, surfatoms)
+    edgeHxyz = get_edge_sites(atoms, surfatoms)
+    hollowHxyz = get_hollow_sites(atoms, surfatoms)
+
+    topH = ase.Atoms('H'*len(topHxyz), topHxyz)
+    edgeH = ase.Atoms('H'*len(edgeHxyz), edgeHxyz)
+    hollowH = ase.Atoms('H'*len(hollowHxyz), hollowHxyz)
+
+    # write structures with all top, edge and hollow sites
+
+
+    pathlib.Path("top").mkdir(parents=True, exist_ok=True) 
+    pathlib.Path("edge").mkdir(parents=True, exist_ok=True) 
+    pathlib.Path("hollow").mkdir(parents=True, exist_ok=True) 
+
+    structure_topH = atoms + topH
+    ase.io.write("top/structure_topH.xyz", structure_topH)
+    structure_edgeH = atoms + edgeH
+    ase.io.write("edge/structure_edgeH.xyz", structure_edgeH)
+    structure_hollowH = atoms + hollowH
+    ase.io.write("hollow/structure_hollowH.xyz", structure_hollowH)
+
+    # write into files with single hydrogen
+    for adatom, idx in zip(topH, range(len(topH))):
+        #print(adatom)
+        structure_H = atoms + adatom
+        dirname = "top/" + "H" + str(idx + 1)
+        pathlib.Path(dirname).mkdir(parents=True, exist_ok=True) 
+        ase.io.write(dirname + "/" + structure_name + "H.xyz", structure_H)
+
+    for adatom, idx in zip(edgeH, range(len(edgeH))):
+        #print(adatom)
+        structure_H = atoms + adatom
+        dirname = "edge/" + "H" + str(idx + 1)
+        pathlib.Path(dirname).mkdir(parents=True, exist_ok=True) 
+        ase.io.write(dirname + "/" + structure_name + "H.xyz", structure_H)
+
+    for adatom, idx in zip(hollowH, range(len(hollowH))):
+        #print(adatom)
+        structure_H = atoms + adatom
+        dirname = "hollow/" + "H" + str(idx + 1)
+        pathlib.Path(dirname).mkdir(parents=True, exist_ok=True) 
+        ase.io.write(dirname + "/" + structure_name + "H.xyz", structure_H)       
+
+
+    return None
+
+
+if __name__ == "__main__":
+    atoms = ase.io.read("h2o2.xyz")
+    atoms = ase.io.read("au40cu40.xyz")
+    """
+    surfatoms = get_surface_atoms(atoms)
+
+    print(surfatoms, surfatoms.shape)
+    surfit = atoms[surfatoms]
+    ase.io.write("surface.xyz", surfit)
+    print("done get surface atoms")
+
+    topHxyz = get_top_sites(atoms, surfatoms)
+    print(topHxyz.shape)
+    topH = ase.Atoms('H'*len(topHxyz), topHxyz)
+    structure_topH = atoms + topH
+    ase.io.write("structure_topH.xyz", structure_topH)
+    print("done get top sites")
+
+
+    edgeHxyz = get_edge_sites(atoms, surfatoms)
+    print(edgeHxyz.shape)
+    
+    #np.savetxt("edgeHxyz.txt",edgeHxyz)
+    edgeH = ase.Atoms('H'*len(edgeHxyz), edgeHxyz)
+    structure_edgeH = atoms + edgeH
+    ase.io.write("structure_edgeH.xyz", structure_edgeH)
+    print("done get edge sites")
+
+
+    hollowHxyz = get_hollow_sites(atoms, surfatoms)
+    print(edgeHxyz.shape)
+    
+    #np.savetxt("edgeHxyz.txt",edgeHxyz)
+    hollowH = ase.Atoms('H'*len(hollowHxyz), hollowHxyz)
+    structure_hollowH = atoms + hollowH
+    ase.io.write("structure_hollowH.xyz", structure_hollowH)
+    print("done get hollow sites")
+    """
+
+    write_all_sites(atoms, "au40cu40")
+
+
+
---
+++ utilities/create_adsite_dirs.py
@@ -0,0 +1,39 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import numpy as np 
+import ase, ase.io
+import os, argparse, time
+from ctypes import *
+import soaplite
+import pathlib
+import clusgeo
+from os.path import basename
+
+if __name__ == "__main__":
+    t0_total = time.time()
+
+    ### INPUT ###
+    parser = argparse.ArgumentParser(description=
+        'Give input xyz filename ')
+    parser.add_argument('arguments', metavar='args', type=str, nargs='+',
+                                   help='[filename]')
+    args = parser.parse_args()
+    print("Passed arguments:", args.arguments)
+    if len(args.arguments) < 1:
+        print('Not enough arguments')
+        exit(1)
+    infilename = args.arguments[0]
+    rootname = basename(infilename)
+    rootname = rootname.replace(".xyz", "")
+    ### PROCESS ###
+
+    atoms = ase.io.read(infilename)
+    clusgeo.write_all_sites(atoms, rootname)
+
+    t1_total = time.time()
+    print("Total run time:", str(t1_total - t0_total))
+
+

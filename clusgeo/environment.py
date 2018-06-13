e6ddbe683e4ca94f003df1bc47477aba360e7ac8
marc.o.j.jaeger <marc.o.j.jaeger@gmail.com>
2018-06-13 16:58:50 +0300

    added functions for environment.py, fps, unique_sites, soapmatrix

--- clusgeo/environment.py
+++ clusgeo/environment.py
@@ -5,6 +5,51 @@ from ctypes import *
 import soaplite
 import pathlib
 import clusgeo.surface
+from scipy.spatial.distance import squareform, pdist
+
+def _fps(pts, K, greedy=False):
+    dist_matrix = squareform(pdist(pts))
+    fts_ids = np.zeros(K, dtype='int')
+     
+    #choosing random start point
+    fts_ids[0] = np.random.choice(pts.shape[0])
+     
+    #finding next k-1
+    if greedy:
+        for i in range(1, K):
+            fts_ids[i] = np.argmax(dist_matrix[fts_ids[i-1]])
+    else:
+        min_dist = dist_matrix[fts_ids[0]]
+        for i in range(1, K):
+            fts_ids[i] = np.argmax(min_dist)
+            min_dist = np.minimum(min_dist, dist_matrix[fts_ids[i]])
+     
+    return fts_ids
+
+def _safe_fps(pts, K, greedy=False):
+    dist_matrix = squareform(pdist(pts))
+    fts_ids = np.zeros(K, dtype='int') -1
+     
+    #choosing random start point
+    fts_ids[0] = np.random.choice(pts.shape[0])
+     
+    #finding next k-1
+    if greedy:
+        for i in range(1, K):
+            arg = np.argmax(dist_matrix[fts_ids[i-1]])
+            if arg in fts_ids:
+                fts_ids[i] = np.argmax(np.isin(np.arange(K), fts_ids, invert=True))
+            else:
+                fts_ids[i] = np.argmax(dist_matrix[fts_ids[i-1]])
+    else:
+        min_dist = dist_matrix[fts_ids[0]]
+        for i in range(1, K):
+            if min_dist.max() < 10e-10:
+                fts_ids[i] = np.argmax(np.isin(np.arange(K), fts_ids, invert=True))
+            else:
+                fts_ids[i] = np.argmax(min_dist)
+                min_dist = np.minimum(min_dist, dist_matrix[fts_ids[i]])   
+    return fts_ids
 
 def get_soap_cluster(obj, only_surface = False, bubblesize = 2.5, 
         rCut=5.0, NradBas=5, Lmax=5, crossOver=True, all_atomtypes=[]):
@@ -42,9 +87,29 @@ def get_unique_sites(soapmatrix, threshold = 0.001, idx=[]):
         print("using ids",len(idx), len(soapmatrix) )
         assert len(idx) == len(soapmatrix), "give a list of indices of length %r" % len(soapmatrix) 
 
+    dist_matrix = squareform(pdist(soapmatrix))
+    K = soapmatrix.shape[0]
+    n_features = soapmatrix.shape[1]
+    fts_ids = np.zeros(K, dtype='int') -1
+     
+    #choosing random start point
+    fts_ids[0] = np.random.choice(soapmatrix.shape[0])
+     
+    #finding next k-1
+    min_dist = dist_matrix[fts_ids[0]]
+    for i in range(1, K):
+        if min_dist.max() / (1.0 * n_features) < threshold:
+            # early stop based on threshold
+            fts_ids = fts_ids[:i]
+            break
+        else:
+            fts_ids[i] = np.argmax(min_dist)
+            min_dist = np.minimum(min_dist, dist_matrix[fts_ids[i]])   
+    return fts_ids
+
 
     idx = np.array(idx, dtype = int)
-    if len(idx) == 0:
+    if len(idx) != 0:
         unique_ids = idx[unique_lst]
     else:
         unique_ids = unique_lst
@@ -52,12 +117,54 @@ def get_unique_sites(soapmatrix, threshold = 0.001, idx=[]):
     return unique_ids
 
 
+def rank_sites(soapmatrix, K = None, idx=[], greedy = False, is_safe = False):
+    """Takes a 2D-array soapmatrix, a uniqueness-threshold and optionally a list of indices as input.
+    Returns a list of indices.
+    """
+    ranked_lst = []
+    if len(idx) == 0:
+        print("no ids given")
+    else:
+        print("using ids",len(idx), len(soapmatrix) )
+        assert len(idx) == len(soapmatrix), "give a list of indices of length %r" % len(soapmatrix) 
+
+    if K == None:
+        # run over all datapoints
+        K = soapmatrix.shape[0]
+
+    print("K for fps", K, "matrix for FPS", soapmatrix.shape)    
+    if is_safe:
+        ranked_lst = _safe_fps(soapmatrix, K, greedy = greedy)
+    else:
+        ranked_lst = _fps(soapmatrix, K, greedy = greedy)
+
+
+
+
+    idx = np.array(idx, dtype = int)
+    if len(idx) != 0:
+        ranked_ids = idx[ranked_lst]
+    else:
+        ranked_ids = ranked_lst
+
+    assert len(ranked_ids) == len(set(ranked_ids)), "Error! Double counting in FPS! Use is_safe = True." 
+
+    return ranked_ids
+
+
+
 
 
 if __name__ == "__main__":
 
     atoms = ase.io.read("tests/au40cu40.xyz")
-    
+    atoms = ase.io.read("tests/Au-icosahedron-3.xyz")
+    #from ase.cluster.icosahedron import Icosahedron
+    #from ase.io import write
+
+    #atoms = Icosahedron('Au', noshells=3)
+    #write('tests/Au-icosahedron-3.xyz', atoms)
+
     soapmatrix = get_soap_cluster(atoms, only_surface=False, bubblesize=2.5, NradBas=10, Lmax =9)
     surfsoapmatrix = get_soap_cluster(atoms, only_surface=True, bubblesize=2.5, NradBas=10, Lmax =9)
 
@@ -69,10 +176,28 @@ if __name__ == "__main__":
 
     print("soap sites shape", sitesoapmatrix.shape)
 
+    # fps ranking testing
+
+    soapmatrix = np.random.rand(5,10)
+    soapmatrix = np.vstack((soapmatrix,soapmatrix))
+    print(soapmatrix.shape)
+
+    ranked_ids = rank_sites(soapmatrix, K = None, idx=[], greedy = True, is_safe = True)
+
+
+
+    print(ranked_ids)
+    print("size of ranked ids:", len(ranked_ids), "set:", len(set(ranked_ids)))
+    assert len(ranked_ids) == len(set(ranked_ids)), "Error! Double counting in FPS!" 
+
     # unique sites testing
 
-    unique_lst = get_unique_sites(soapmatrix, idx=surfatoms)
+    unique_lst = get_unique_sites(sitesoapmatrix, idx=surfatoms)
 
+    unique_pos = sitepositions[unique_lst]
+    adsorbates = ase.Atoms('H' * len(unique_lst), unique_pos)
+    h_structure = atoms + adsorbates
 
+    #ase.io.write("uniqueH.xyz", h_structure)
 
-
+    print(unique_lst.shape)

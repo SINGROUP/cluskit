6340dfb1669b387530c05adeca1c0c6af7bdcbeb
Aki78 <kmorooka78@gmail.com>
2018-07-03 15:12:10 +0300

    small clean up

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,8 @@
 import os,sys,inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+#parentdir = os.path.dirname(currentdir)
+#sys.path.insert(0,parentdir)
 
 import clusgeo.surface, clusgeo.environment
 import ase
